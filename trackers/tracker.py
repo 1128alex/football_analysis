b6061febaa51a3865e7baaf1d89a6414c618b07c
Alex Lee <1128alex@gmail.com>
2025-04-30 18:31:29 -0400

    tracker

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -1,5 +1,7 @@
 from ultralytics import YOLO
 import supervision as sv
+import pickle
+import os
 
 class Tracker:
     # model assignment
@@ -19,15 +21,23 @@ class Tracker:
             # conf: confidence threshold
             detections_batch = self.model.predict(frames[i:i+batch_size], conf=0.1)
             detections += detections_batch
-            break
         return detections
 
     # Tracking
-    def get_object_tracks(self, frames):
+    def get_object_tracks(self, frames, read_from_stub=False, stub_path=None):
+        # reading
+        if read_from_stub and stub_path is not None and os.path.exists(stub_path):
+            # if it exists, open up the stub file and load the tracks and return
+            with open(stub_path, 'rb') as f:
+                tracks = pickle.load(f)
+            return tracks
+        
         detections = self.detect_frames(frames)
 
         tracks={
-            "players":[],
+            # [{0:{"bbox":[0,0,0,0]]}, {1:{"bbox":[0,0,0,0]]}, {2:{"bbox":[0,0,0,0]]}},
+            # {{10:{"bbox":[0,0,0,0]]},{21:{"bbox":[0,0,0,0]]},{1:{"bbox":[0,0,0,0]]}}]
+            "players":[], 
             "referees":[],
             "ball":[],
         }
@@ -58,5 +68,30 @@ class Tracker:
             tracks["referees"].append({})
             tracks["ball"].append({})
 
+            # Loop over each detection for players and referees
+            for frame_detections in detection_with_tracks:
+                bbox = frame_detection[0].tolist()
+                cls_id = frame_detection[3]
+                track_id = frame_detection[4]
 
-            print(detection_supervision)
+                if cls_id == cls_names_inv["player"]:
+                    tracks["players"][frame_num][track_id] = {"bbox":bbox}
+
+                if cls_id == cls_names_inv["referee"]:
+                    tracks["referee"][frame_num][track_id] = {"bbox":bbox}
+
+            # Loop over each detection for balls
+            for frame_detections in detection_with_tracks:
+                bbox = frame_detection[0].tolist()
+                cls_id = frame_detection[3]
+
+                if cls_id == cls_names_inv["ball"]:
+                    tracks["ball"][frame_num][1] = {"bbox":bbox}
+        
+        # save
+        if stub_path is not None:
+            # wb: write bites
+            with open(stub_path, 'wb') as f:
+                pickle.dump(tracks, f)  
+
+        return tracks
