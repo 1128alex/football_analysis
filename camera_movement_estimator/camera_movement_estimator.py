0aaaa0674089bd9cc25396f782c958bb6d603b03
Alex Lee <1128alex@gmail.com>
2025-05-02 18:20:52 +0900

    camera movement

--- camera_movement_estimator/camera_movement_estimator.py
+++ camera_movement_estimator/camera_movement_estimator.py
@@ -1,6 +1,7 @@
 import pickle
 import cv2
 import numpy as np
+import os
 import sys
 sys.path.append('../')
 from utils import measure_distance
@@ -31,8 +32,11 @@ class CameraMovementEstimator():
 
     def get_camera_movement(self, frames, read_from_stub = False, stub_path = None):
         # Read the stub
+        if read_from_stub and stub_path is not None and os.path.exists(stub_path):
+            with open(stub_path, 'rb') as f:
+                return pickle.load(f)
 
-        camera_movement = [[0,0]*len(frames)]
+        camera_movement = [[0,0]]*len(frames)
 
         # call all the previous frames as old
         old_gray = cv2.cvtColor(frames[0],cv2.COLOR_BGR2GRAY)
@@ -45,7 +49,7 @@ class CameraMovementEstimator():
             max_distance = 0
             camera_movement_x, camera_movement_y = 0,0
 
-            for i, (new, old) in enumerate(new_featuers, old_features):
+            for i, (new, old) in enumerate(zip(new_featuers, old_features)):
                 new_features_point = new.ravel()
                 old_features_point = old.ravel()
 
@@ -61,3 +65,19 @@ class CameraMovementEstimator():
             old_gray = frame_gray.copy()
 
         return camera_movement
+
+    def draw_camera_movement(self, frame, camera_movement_per_frame):
+        output_frames = []
+        for frame_num, frame in enumerate(frames):
+            frame = frame.copy()
+            cv2.rectangle(overlay, (0,0), (500, 100), (255,255,255), -1)
+            alpha = 0.6
+            cv2.addWeighted(overlay, alpha, frame, 1-alpha, 0, frame)
+
+            x_movement, y_movement = camera_movement_per_frame[frame_num]
+            frame = cv2.putText(frame, f"Camera Movement x: {x_movement:.2f}", (10,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0),3)
+            frame = cv2.putText(frame, f"Camera Movement y: {y_movement:.2f}", (10,60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0),3)
+
+            output_frames.append(frame)
+        
+        return output_frames
