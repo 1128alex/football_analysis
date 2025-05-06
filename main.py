d5ba486da49b6a89198e9836d5be282c461f9066
Alex Lee <1128alex@gmail.com>
2025-05-06 15:24:58 +0900

    speed distance calculation

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from utils import read_video, save_video
 from trackers import Tracker
-import cv2
 import numpy as np
 from team_assigner import TeamAssigner
 from player_ball_assigner import PlayerBallAssigner
 from camera_movement_estimator import CameraMovementEstimator
 from view_transformer import ViewTransformer
+from speed_distance_estimator import SpeedDistanceEstimator
 
 
 def main():
@@ -30,6 +30,10 @@ def main():
 
     # Interpolate ball positions
     tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
+
+    # Speed and distance estimator
+    speed_and_distance_estimator = SpeedDistanceEstimator()
+    speed_and_distance_estimator.add_speed_and_distance_to_tracks(tracks)
 
     # Assign player teams
     team_assigner = TeamAssigner()
@@ -71,6 +75,9 @@ def main():
     output_video_frames = camera_movement_estimator.draw_camera_movement(
         output_video_frames, camera_movement_per_frame
     )
+
+    ## Draw Speed and Distance
+    speed_and_distance_estimator.draw_speed_and_distance(output_video_frames, tracks)
 
     # Save Video
     save_video(output_video_frames, "output_videos/output_video.avi")
--- team_assigner/team_assigner.py
+++ team_assigner/team_assigner.py
@@ -1,5 +1,6 @@
 from sklearn.cluster import KMeans
 
+
 class TeamAssigner:
     def __init__(self):
         self.team_colors = {}
@@ -11,14 +12,14 @@ class TeamAssigner:
         image_2d = image.reshape(-1, 3)
 
         # Preform k-means with 2 clusters
-        kmeans = KMeans(n_clusters = 2, init = "k-means++", n_init = 10)
+        kmeans = KMeans(n_clusters=2, init="k-means++", n_init=10)
         kmeans.fit(image_2d)
 
         return kmeans
 
     def get_player_color(self, frame, bbox):
-        image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
-        top_half_image = image[0:int(image.shape[0] / 2), :]
+        image = frame[int(bbox[1]) : int(bbox[3]), int(bbox[0]) : int(bbox[2])]
+        top_half_image = image[0 : int(image.shape[0] / 2), :]
 
         # Get clustering model
         kmeans = self.get_clustering_model(top_half_image)
@@ -27,9 +28,16 @@ class TeamAssigner:
         labels = kmeans.labels_
 
         # Reshape the labels to teh image shape
-        clustered_image = labels.reshape(top_half_image.shape[0], top_half_image.shape[1])
+        clustered_image = labels.reshape(
+            top_half_image.shape[0], top_half_image.shape[1]
+        )
 
-        corner_clusters = [clustered_image[0,0], clustered_image[0,-1], clustered_image[-1,0], clustered_image[-1,-1]]
+        corner_clusters = [
+            clustered_image[0, 0],
+            clustered_image[0, -1],
+            clustered_image[-1, 0],
+            clustered_image[-1, -1],
+        ]
         non_player_cluster = max(set(corner_clusters), key=corner_clusters.count)
         player_cluster = 1 - non_player_cluster
 
@@ -45,11 +53,11 @@ class TeamAssigner:
             player_colors.append(player_color)
 
         # divide the color into two
-        kmeans = KMeans(n_clusters = 2, init = "k-means++", n_init = 1)
+        kmeans = KMeans(n_clusters=2, init="k-means++", n_init=1)
         kmeans.fit(player_colors)
-        
+
         self.kmeans = kmeans
-        
+
         self.team_colors[1] = kmeans.cluster_centers_[0]
         self.team_colors[2] = kmeans.cluster_centers_[1]
 
@@ -70,4 +78,3 @@ class TeamAssigner:
         self.player_team_dict[player_id] = team_id
 
         return team_id
-
